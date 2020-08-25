f139c74b0a344139503ab735e83879686afaf5e2
dcargs <cargilldevin@gmail.com>
2020-08-24 23:22:09 -0500

    updated scripts to point where there's no database

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # stock-db
 Docker Stock Discord Bot DB
+
+## How to
+- To create a fresh database with no data
+```
+./setup.sh
+```
+- To upgrade an existing database
+```
+./upgrade.sh
+```
--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,21 @@
 docker stop stock-db
 docker rm stock-db
-git pull origin master
+git pull origin setup
 docker pull mysql/mysql-server
 docker run --name=stock-db -d mysql/mysql-server
 echo "Sleeping for 10 seconds to let the instance start"
+
 sleep  10
-docker logs stock-db 2>&1 | grep GENERATE
+password_str=`docker logs stock-db 2>&1 | grep GENERATE`
+# Set space as the delimiter
+IFS=' '
+read -a password_arr <<< "$password_str"
+password=${password_arr[4]}
+
+read -sp 'Root Password: ' new_password
 echo ""
+
+docker exec -it stock-db mysql --user="root" --password="$password" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '$new_password';" --connect-expired-password
+
 echo "Use this command to login to MySQL:"
 echo "docker exec -it stock-db mysql -uroot -p"
